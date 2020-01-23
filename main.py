be3fe1306495af7eb85c10f857f3af39383396be
BastienG <bastien.genestal@epitech.eu>
2020-01-23 13:56:02 +0100

    test heroku

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from DarwinBot import DarwinBot
 import os
 
 client = DarwinBot(command_prefix='')
-
+e;azedsqd e
 client.run(os.environ.get('BOT_TOKEN'))
 #client.run('NjY5NDkxNTM0NDMyOTYwNTIy.XimSEA.pxpRGpvqPqwIH9aZ9EM3ZAzOKz8')
