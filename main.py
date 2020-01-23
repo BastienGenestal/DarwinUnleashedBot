32e0edfb49a8200d2ede41cb9eed8ddbda28ce48
BastienG <bastien.genestal@epitech.eu>
2020-01-23 13:57:33 +0100

    print debug keroju

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from DarwinBot import DarwinBot
 import os
 
+print("Starting the bot...")
 client = DarwinBot(command_prefix='')
-e;azedsqd e
 client.run(os.environ.get('BOT_TOKEN'))
 #client.run('NjY5NDkxNTM0NDMyOTYwNTIy.XimSEA.pxpRGpvqPqwIH9aZ9EM3ZAzOKz8')
